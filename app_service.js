3825328b4defd1bf50f1784a198192c9f9363c24
NevanSonic <nevansonic@gmail.com>
2026-06-20 11:37:17 +0530

    Revert "Merge pull request #9 from nevan-sonic/fix/db-pool-exhaustion-6tra"
    This reverts commit 65f1318b70bc58db2821d889c1da7696fa530959, reversing
    changes made to 0bccf2ab392bab86952e62f409f3fb605ca0beaa.

--- app_service.js
+++ app_service.js
@@ -3,10 +3,10 @@ const poolConfig = {
   host: "localhost",
   port: 5432,
   database: "production_db",
-  // Increased database connection limit to alleviate connection timeouts and deadlocks
-  max: 100,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 10000,
+  // Database connection limit
+  max: 50,
+  idleTimeoutMillis: 10000,
+  connectionTimeoutMillis: 5000,
 };
 const dbPool = new Pool(poolConfig);
 module.exports = { dbPool, poolConfig };
