a4ebe7e8d78952494c3f63ec28af0d273b0a46e3
NevanSonic <nevansonic@gmail.com>
2026-06-22 20:59:07 +0530

    chore: remove debugging and development utility scripts

--- scripts/inspect-function.js
+++
@@ -1,6 +0,0 @@
-async function main() {
-    const sdk = await import("@terminal3/t3n-sdk");
-    const fnStr = sdk.T3nClient.prototype.execute.toString();
-    console.log("execute implementation:\n", fnStr);
-}
-main();
